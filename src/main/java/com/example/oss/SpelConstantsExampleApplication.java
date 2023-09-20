c697909db14c97f5fe806c5c96ddbda301663dce
os2506 <said-souilem@live.fr>
2023-09-20 07:00:26 +0200

    last update

--- src/main/java/com/example/oss/SpelConstantsExampleApplication.java
+++ src/main/java/com/example/oss/SpelConstantsExampleApplication.java
@@ -4,20 +4,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
-
 @SpringBootApplication
 public class SpelConstantsExampleApplication {
-	
-    public static void main(String[] args) {
-	// Create the SpEL parser
-	ExpressionParser parser = new SpelExpressionParser();
-	
-	double piival = (double) parser.parseExpression("T(com.example.oss.entity.LogicConstants).PI").getValue();
-	String code = (String) parser.parseExpression("T(com.example.oss.entity.LogicConstants).code_test").getValue();
 
-	System.out.println("Constant val: " + piival);
-	System.out.println("Constant code: " + code);
-    
-    }
+	public static void main(String[] args) {
+		// Create the SpEL parser
+		ExpressionParser parser = new SpelExpressionParser();
+
+		double piival = (double) parser.parseExpression("T(com.example.oss.entity.LogicConstants).PI").getValue();
+		String code = (String) parser.parseExpression("T(com.example.oss.entity.LogicConstants).code_test").getValue();
+
+		System.out.println("Constant val: " + piival);
+		System.out.println("Constant code: " + code);
+
+	}
 
 }
